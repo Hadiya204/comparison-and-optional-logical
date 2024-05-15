aa088098df53dd86966d71e9e5bd2bbc962238b6
Hadiya204 <168410223+Hadiya204@users.noreply.github.com>
2024-05-15 16:13:13 +0500

    Add files via upload

---
+++ javascript h/index.js
@@ -0,0 +1,85 @@
+// class activity
+// question:01
+let age = 20; 
+
+if (age >= 18) {
+    console.log('You are eligible to vote.');
+}
+else {
+    console.log('You are not eligible to vote yet.');
+}
+// question:02
+let temperature = 28; 
+
+if (temperature > 25) {
+    console.log("It's a hot day!");
+}
+else {
+    console.log("It's a normal day.");
+}
+// question:03
+let student_grade = +prompt("enter your score");
+
+if (student_grade >= 60) {
+    console.log('You passed the test!');
+}
+else {
+    console.log('You failed the test.');
+}
+// question:04
+let is_raining = true;
+
+if (is_raining) {
+    console.log('Remember to bring an umbrella.');
+} else {
+    console.log('Enjoy the sunny weather!');
+}
+//question:05
+let student__age = +prompt("enter your age")
+if (student__age>18) {
+    console.log("You are an adult");    
+}
+else if(student__age<18){
+    console.log("You are a minor");
+}
+//question:06
+let number = +prompt("enter the number")
+if (number > 0) {
+    console.log("The number is positive");
+    }
+ else if(number == 0){
+    console.log( "The number is negative");
+}
+else if (number < 0) {
+    console.log("The number is negative");
+}
+else{
+    console.log("all number are integers");
+}
+// class work
+// logical operation
+let score = +prompt("enter your score")
+if(score >= 80 || score >= 60){
+    console.log("you passed");
+}else{
+console.log("you failed");
+}
+let issunny = true;
+let iswarm = false;
+if (issunny && iswarm) {
+    console.log("perfect weather for outdoor activities!");
+}else{
+    console.log("may be another day.");
+}
+let israining = false;
+if(israining){
+    console.log("it's not raining. enjoy the day!");
+}else{
+    console.log("don't forget your umbrella");
+}
+
+
+
+
+
+
